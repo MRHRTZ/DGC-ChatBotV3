46bad1088508c228dece9ff590609ea5916ac946
MRHRTZ <hanifsyauqi61@gmail.com>
2020-10-27 21:31:47 -0700

    Ubah gaes

--- start_bot.sh
+++
@@ -1,7 +0,0 @@
-#usr/bin/bash
-echo "Gunakan ubuntu atau linux versi lain untuk memudahkan pemakaian bot, tidak bisa digunakan di termux!"
-chmod +x whatsapp-bot
-cd whatsapp bot
-sudo apt-get install nodejs -y
-npm i
-npm start
