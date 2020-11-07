4233aa784bc29328106d53b5aea88e187cca8d3e
MRHRTZ <hanifsyauqi61@gmail.com>
2020-11-06 16:49:53 -0800

    Prepare Update open-wa

--- options.js
+++ options.js
@@ -28,21 +28,24 @@ module.exports = options = (headless, start) => {
         qrTimeout: 0,
         authTimeout: 0,
         autoRefresh: true,
-        restartOnCrash: true,
+        skipBrokenMethodsCheck: true, 
+        browserRevision: 800218,
+        restartOnCrash: start,
+        logConsole: true,
         cacheEnabled: false,
-        executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', 
+        // executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe', 
         useChrome: true,
         killProcessOnBrowserClose: true,
         throwErrorOnTosBlock: false,
-        // chromiumArgs: [
-        //     '--no-sandbox',
-        //     '--disable-setuid-sandbox',
-        //     '--aggressive-cache-discard',
-        //     '--disable-cache',
-        //     '--disable-application-cache',
-        //     '--disable-offline-load-stale-cache',
-        //     '--disk-cache-size=0'
-        // ]
+        chromiumArgs: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--aggressive-cache-discard',
+            '--disable-cache',
+            '--disable-application-cache',
+            '--disable-offline-load-stale-cache',
+            '--disk-cache-size=0'
+        ]
     }
     return options
 }
