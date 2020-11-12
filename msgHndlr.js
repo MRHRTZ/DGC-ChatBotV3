e8dfee5913ea90671286ed39ab2b04da0f58ea5b
MRHRTZ <hanifsyauqi61@gmail.com>
2020-11-12 00:53:44 -0800

    Jodohkanlah aku dengannya;)

--- msgHndlr.js
+++ msgHndlr.js
@@ -100,10 +100,10 @@ module.exports = msgHandler = async (client, message) => {
         const isNsfw = isGroupMsg ? nsfw_.includes(chat.id) : false
         const uaOverride = 'WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
         const isUrl = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/gi)
-        if (!isGroupMsg && command.startsWith('!')) console.log('\x1b[1;31m~\x1b[1;37m>', '[\x1b[1;32mEXEC\x1b[1;37m]', time, color(msgs(command)), 'from', color(pushname))
-        if (isGroupMsg && command.startsWith('!')) console.log('\x1b[1;31m~\x1b[1;37m>', '[\x1b[1;32mEXEC\x1b[1;37m]', time, color(msgs(command)), 'from', color(pushname), 'in', color(formattedTitle))
-        if (!isGroupMsg && !command.startsWith('!')) console.log('\x1b[1;33m~\x1b[1;37m>', '[\x1b[1;31mMSG\x1b[1;37m]', time, color('message'), 'from', color(pushname))
-        if (isGroupMsg && !command.startsWith('!')) console.log('\x1b[1;33m~\x1b[1;37m>', '[\x1b[1;31mMSG\x1b[1;37m]', time, color('message'), 'from', color(pushname), 'in', color(formattedTitle))
+        if (!isGroupMsg && command.startsWith('!')) console.log('\x1b[1;31m~\x1b[1;37m>>', '[\x1b[1;32mOUT\x1b[1;37m]', time, color(msgs(command)), 'from', color(pushname))
+        if (isGroupMsg && command.startsWith('!')) console.log('\x1b[1;31m~\x1b[1;37m>>', '[\x1b[1;32mOUT\x1b[1;37m]', time, color(msgs(command)), 'from', color(pushname), 'in', color(formattedTitle))
+        if (!isGroupMsg && !command.startsWith('!')) console.log('\x1b[1;33m~\x1b[1;37m>>', '[\x1b[1;31mMSG\x1b[1;37m]', time, color('pesan'), 'from', color(pushname))
+        if (isGroupMsg && !command.startsWith('!')) console.log('\x1b[1;33m~\x1b[1;37m>>', '[\x1b[1;31mMSG\x1b[1;37m]', time, color('pesan'), 'from', color(pushname), 'in', color(formattedTitle))
         if (isBlocked ) return 
             //client.reply(from, `_Sepertinya anda telah terblokir dikarenakan vc/call bot._`)
         //if (!isOwner) return
@@ -266,9 +266,9 @@ module.exports = msgHandler = async (client, message) => {
         if (!isMuted(chat.id) == true) return console.log(`Muted ${chat.id} ${name}`)
         //client.reply(from, `_Hai ${pushname} Limit request anda sudah mencapai batas, Coba lagi besok..._`, id)
 
-        if (body == "Assalamualaikum" || body == "Assalamu'alaikum" || body == "Samlikum" || body == "Samlekum"){
-                return client.reply(from, "Wa'alaikumsalam Warahmatullahi Wabarokatuh", id)
-        } else if (body == "Anjing" || body == "Goblok" || body == "Ngentod" || body == "Bangsat"){
+        if (args[0] == "Assalamualaikum" || args[0] == "Assalamu'alaikum" || args[0] == "Samlikum" || args[0] == "Samlekom"){
+                return client.reply(from, "Wa'alaikumsalam warahmatullahi wabarokatuh", id)
+        } else if (args[0] == "Anjing" || args[0] == "Goblok" || args[0] == "Ngentod" || args[0] == "Bangsat"){
             if (!isGroupAdmins) {
                 return client.reply(from, "WOYY JANGAN TOXIC MEMBER BANGSAT SETAN!", id)
                 .then(() => client.removeParticipant(groupId, sender.id))
@@ -2761,7 +2761,7 @@ Nomor : wa.me/${hapusser[0]}
             // console.log(isBotGroupAdmins)
             //const telahfixorg = orang.replace('+', '')
             try {
-                await client.addParticipant(from,`${orang}@c.us`).catch((err) => console.log(err))
+                await client.addParticipant(from, `${orang}@c.us`).catch((err) => console.log(err))
                 // console.log(orang)
             } catch {
                 client.reply(from, mess.error.Ad, id)
